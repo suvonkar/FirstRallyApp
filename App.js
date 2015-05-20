c261bef7689877e972cf1413f99cbac7e24199ad
suvonkar <suvonkar.dam@lexmark.com>
2015-05-20 16:35:08 +0530

    Assignment completed

--- App.js
+++ App.js
@@ -1,35 +1,49 @@
-Ext.define('CustomApp', {
-    extend: 'Rally.app.App',
-    componentCls: 'app',
-    launch: function() {
-        console.log("Hello World....");
-        this._loadData();
-        console.log("what is this?" , this);
-    },
-    
-    _loadData: function(){
-        console.log("Inside dataload");
-        var myStore = Ext.create("Rally.data.wsapi.Store", {
-            model: 'User Story',
-            autoLoad: true,
-            listeners: {
-                load: function(myStore, myData, success) {
-                    console.log("my store is ", myStore);
-                    console.log("my data is ", myData);
-                    console.log("this is " , this);
-                    this._loadDataGrid(myStore);
-                },
-                scope: this
-            },
-            fetch: ['FormattedID','Name', 'ScheduleState']
-        });
-    },
-    
-    _loadDataGrid:function(myDataStore){
-        var myGrid = Ext.create('Rally.ui.grid.Grid', {
-            store: myDataStore,
-            columnCfgs:['FormattedID','Name', 'ScheduleState']
-         });
-         this.add(myGrid);
-    }
-});
+Ext.define('CustomApp',
+		{
+			extend : 'Rally.app.App',
+			componentCls : 'app',
+			launch : function() {
+				this._loadData();
+			},
+
+			_loadData : function() {
+				console.log("Inside dataload");
+				var myStore = Ext.create("Rally.data.wsapi.Store", {
+					model : 'User Story',
+					autoLoad : true,
+					listeners : {
+						load : function(myStore, myData, success) {
+							console.log("my store is ", myStore);
+							console.log("my data is ", myData);
+							this._loadDataGrid(myStore);
+						},
+						scope : this
+					},
+					fetch : [ 'FormattedID', 'Name', 'Parent', 'Owner',
+							'InProgressDate', 'ScheduleState', 'Project',
+							'c_KanbanState' ],
+					context : {
+						project : Rally.environment.getContext().getProject()
+					},
+					filters : [ {
+						property : 'c_KanbanState',
+						operator : '=',
+						value : 'Backlog'
+					} ],
+					sorters : [ {
+						property : 'InProgressDate',
+						direction : 'ASC'
+					} ]
+				});
+			},
+
+			_loadDataGrid : function(myDataStore) {
+				var myGrid = Ext.create('Rally.ui.grid.Grid', {
+					store : myDataStore,
+					columnCfgs : [ 'FormattedID', 'Name', 'Parent', 'Owner',
+							'ScheduleState', 'Project', 'InProgressDate',
+							'c_KanbanState' ]
+				});
+				this.add(myGrid);
+			}
+		});
